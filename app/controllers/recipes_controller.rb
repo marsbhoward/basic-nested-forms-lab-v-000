b9a807662062df5b6dae9ac5f9583f98d7b5c501
Mars Howard <marsbhoward@gmail.com>
2019-01-26 22:31:40 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,16 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'sugar')
+    @recipe.ingredients.build(name: 'salt')
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :ingredients_attributes => [:name, :quantity])
   end
 end
